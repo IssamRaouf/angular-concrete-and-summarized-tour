1535e758ab2f2453322df50afef768436aa3fa53
iraoufSoft <issam.raouf.dev@gmail.com>
2019-08-18 17:52:06 +0100

    init lifecycle hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {TodoComponent} from './1-content-projection/todo/todo.component';
-import {TodoListComponent} from './1-content-projection/todo-list/todo-list.component';
-import {TodoFormComponent} from './1-content-projection/todo-form/todo-form.component';
+
+import {TodoComponent, TodoListComponent, TodoFormComponent} from './index';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 
---
+++ src/app/index.ts
@@ -0,0 +1,15 @@
+
+// 1 - Projection Content
+
+/*
+
+export {TodoComponent} from './1-content-projection/todo/todo.component';
+export {TodoListComponent} from './1-content-projection/todo-list/todo-list.component';
+export {TodoFormComponent} from './1-content-projection/todo-form/todo-form.component';
+*/
+
+
+// 2 - Lifecycle Hooks
+export {TodoComponent} from './2-lifecycle-hooks/todo/todo.component'
+export {TodoListComponent} from './2-lifecycle-hooks/todo-list/todo-list.component';
+export {TodoFormComponent} from './2-lifecycle-hooks/todo-form/todo-form.component';
