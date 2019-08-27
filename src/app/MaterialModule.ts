09f59a7bcd961724000de0517387d2c19c523fcf
iraoufSoft <issam.raouf.dev@gmail.com>
2019-08-27 16:15:52 +0100

    #10 intergration dashboard & lazy loading

--- src/app/MaterialModule.ts
+++ src/app/MaterialModule.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule, MatInputModule, MatCardModule, MatSelectModule,MatFormFieldModule } from '@angular/material';
+import {NgModule} from '@angular/core';
+import {
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule
+} from '@angular/material';
+
 @NgModule({
     exports: [
         MatButtonModule,
         MatInputModule,
         MatCardModule,
-        MatSelectModule
-    ],
-    imports: [
-        MatButtonModule,
-        MatInputModule,
-        MatCardModule,
-        MatSelectModule
+        MatSelectModule,
+        MatFormFieldModule,
+        MatToolbarModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatExpansionModule
+
     ]
 })
 export class MaterialModule {
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
 const routes: Routes = [
-  {
-    path: 'content-projection',
-    loadChildren: () => import('./1-components/1-content-projection/content-projection.module').then(mod => mod.ContentProjectionModule)
-  }
+    {
+        path: 'content-projection',
+        loadChildren: () => import('./1-components/1-content-projection/content-projection.module').then(mod => mod.ContentProjectionModule)
+    },
+    {
+        path: 'lifecycle-hooks',
+        loadChildren: () => import('./1-components/2-lifecycle-hooks/lifecycle-hooks.module').then(mod => mod.LifecycleHooksModule)
+    },
+    {
+        path: 'view-content-child-children',
+        loadChildren: () => import('./1-components/3-view_children-content_children/view-content-child-children.module')
+            .then(mod => mod.ViewContentChildChildrenModule)
+    },
+    {
+        path: '',
+        redirectTo: 'content-projection',
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
