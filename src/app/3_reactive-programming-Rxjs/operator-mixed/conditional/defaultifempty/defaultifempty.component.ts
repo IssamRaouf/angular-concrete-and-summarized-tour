4c327ddf9d4371cc35518215e9a5c30b5b96857e
iraoufSoft <issam.raouf.dev@gmail.com>
2019-10-24 21:42:50 +0100

    #2 conditional init defaultIfEmpty

---
+++ src/app/3_reactive-programming-Rxjs/operator-mixed/conditional/defaultifempty/defaultifempty.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {of} from 'rxjs';
+import {defaultIfEmpty} from 'rxjs/operators';
+
+@Component({
+    selector: 'app-defaultifempty',
+    templateUrl: './defaultifempty.component.html',
+    styleUrls: ['./defaultifempty.component.scss']
+})
+export class DefaultifemptyComponent implements OnInit {
+
+    constructor() {
+
+
+    }
+
+    ngOnInit() {
+        const observ = of().pipe(defaultIfEmpty('hello world'));
+        observ.subscribe(res => console.log('Result is :', res));
+    }
+
+}
--- src/app/3_reactive-programming-Rxjs/rp-rxjs.module.ts
+++ src/app/3_reactive-programming-Rxjs/rp-rxjs.module.ts
@@ -19,12 +19,15 @@ import {RaceComponent} from './operator-mixed/combination/race/race.component';
 import {ForkjoinComponent} from './operator-mixed/combination/forkjoin/forkjoin.component';
 import {MergeallComponent} from './operator-mixed/combination/mergeall/mergeall.component';
 import {ZipComponent} from './operator-mixed/combination/zip/zip.component';
+import {ConditionalComponent} from './operator-mixed/conditional/conditional.component';
+import {DefaultifemptyComponent} from './operator-mixed/conditional/defaultifempty/defaultifempty.component';
 
 @NgModule({
     imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, RpRxjsRoutingModule],
     declarations: [TodoComponent, CreationComponent, PipeComponent, OperatorMixedComponent,
         CombinationComponent, CombinelatestComponent, ConcatComponent, StartwithComponent
-        , EndwithComponent, MergeComponent, PairwiseComponent, RaceComponent, ForkjoinComponent, MergeallComponent, ZipComponent],
+        , EndwithComponent, MergeComponent, PairwiseComponent, RaceComponent, ForkjoinComponent, MergeallComponent, ZipComponent,
+        ConditionalComponent, DefaultifemptyComponent],
     providers: [ServiceMockDataService]
 })
 export class RpRxjsModule {
