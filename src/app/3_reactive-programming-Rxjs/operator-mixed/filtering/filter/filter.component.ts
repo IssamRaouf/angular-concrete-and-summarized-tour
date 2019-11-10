8e3c4bcb8ad0b67404326520c6ebcf0228517709
iraoufSoft <issam.raouf.dev@gmail.com>
2019-11-10 14:35:05 +0100

    rxjs filter

---
+++ src/app/3_reactive-programming-Rxjs/operator-mixed/filtering/filter/filter.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {of} from 'rxjs';
+import {filter} from 'rxjs/operators';
+
+@Component({
+    selector: 'app-filter',
+    templateUrl: './filter.component.html',
+    styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        const source = of(1, 2, 3, 4, 5, 6);
+        const result = source.pipe(filter(num => num % 2 === 0));
+
+       // result.subscribe(res => console.log('Result : ', res));
+
+
+
+    }
+
+}
