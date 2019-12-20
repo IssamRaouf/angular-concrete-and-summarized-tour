110e91d013ee2d60128f45e211acee6371e5e123
iraoufSoft <issam.raouf.dev@gmail.com>
2019-12-20 13:20:08 +0100

    pipes title case

--- src/app/4_pipes/1-pipes-important-exist/todo/todo.component.ts
+++ src/app/4_pipes/1-pipes-important-exist/todo/todo.component.ts
@@ -8,6 +8,19 @@ import {Component, OnInit} from '@angular/core';
 
 export class TodoComponent implements OnInit {
 
+    public titleCase: string = '{{\'issam raouf\' | titlecase}}';
+    public upperCase: string = '{{\'issam raouf\' | uppercase}}';
+    public lowerCase: string = '{{\'ISSAM RAOUF\' | lowercase}}';
+    public i18NSelect: string = '{{\'other\' | i18nSelect:inviteMap}}';
+    public i18NPlural: string = '{{ 2 | i18nPlural: messageMapping }}';
+    public json: string = '{{inviteMap | json}}';
+
+
+    public gender: string = 'other';
+    public inviteMap: any = {male: 'Invite-le.', female: 'Invite-la.', other: 'Invite-les.'};
+
+    public messageMapping: { [k: string]: string } = {'=0': 'No message.', '=1': 'One message', 'other': '# messages .'};
+
 
     constructor() {
 
