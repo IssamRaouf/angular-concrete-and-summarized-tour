f71c526ce61eb19a98c1df2f9be6f78fb212043a
iraoufSoft <issam.raouf.dev@gmail.com>
2019-12-21 12:55:44 +0100

    pipe number decimalPipe

--- src/app/4_pipes/1-pipes-important-exist/note.md
+++ src/app/4_pipes/1-pipes-important-exist/note.md
@@ -4,7 +4,8 @@
 - Ils sont un moyen simple de transformer des valeurs dans un modèle angulaire.<br> 
 - Il existe des tuyaux intégrés <br>
 - Mais vous pouvez également créer vos propres tuyaux. <br> 
-- Un tuyau prend une ou plusieurs valeurs, puis renvoie une valeur. Ceci est idéal pour les transformations simples sur les données, mais il peut également être utilisé d'autres manières uniques,Par exmple pour filtrer ...
+- Un tuyau prend une ou plusieurs valeurs, puis renvoie une valeur. Ceci est idéal pour les transformations simples sur les données,
+ mais il peut également être utilisé d'autres manières uniques,Par exmple pour filtrer ...
 
 
 #### Exemples :
--- src/app/4_pipes/1-pipes-important-exist/todo/todo.component.ts
+++ src/app/4_pipes/1-pipes-important-exist/todo/todo.component.ts
@@ -7,18 +7,19 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class TodoComponent implements OnInit {
-
+    // description
     public titleCase: string = '{{\'issam raouf\' | titlecase}}';
     public upperCase: string = '{{\'issam raouf\' | uppercase}}';
     public lowerCase: string = '{{\'ISSAM RAOUF\' | lowercase}}';
     public i18NSelect: string = '{{\'other\' | i18nSelect:inviteMap}}';
     public i18NPlural: string = '{{ 2 | i18nPlural: messageMapping }}';
     public json: string = '{{inviteMap | json}}';
+    public decimalPipe: string = '{{numberFt | number:\'1.1-6\'}}';
 
 
-    public gender: string = 'other';
+    // vars
+    public numberFt = 8.718281828459045;
     public inviteMap: any = {male: 'Invite-le.', female: 'Invite-la.', other: 'Invite-les.'};
-
     public messageMapping: { [k: string]: string } = {'=0': 'No message.', '=1': 'One message', 'other': '# messages .'};
 
 
