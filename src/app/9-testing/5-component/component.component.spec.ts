92dc827b3c5287f1d7775f2745af7215c6e782e3
Issam Raouf <issam.raouf@labonneagence.com>
2020-02-09 14:41:59 +0100

    testing

---
+++ src/app/9-testing/5-component/component.component.spec.ts
@@ -0,0 +1,63 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ComponentComponent} from './component.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+import 'zone.js/dist/zone-testing';
+import {UserModel} from '../models/user.model';
+
+describe('ComponentComponent', () => {
+    let component: ComponentComponent;
+    let fixture: ComponentFixture<ComponentComponent>;
+    let usernameEl: DebugElement;
+    let passwordEl: DebugElement;
+    let submitEl: DebugElement;
+    // environment clear and init
+    beforeAll(() => {
+        TestBed.resetTestEnvironment();
+        TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+            platformBrowserDynamicTesting());
+    });
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ComponentComponent]
+        });
+        fixture = TestBed.createComponent(ComponentComponent);
+        component = fixture.componentInstance;
+
+        usernameEl = fixture.debugElement.query(By.css('input[type=email]'));
+        passwordEl = fixture.debugElement.query(By.css('input[type=password]'));
+        submitEl = fixture.debugElement.query(By.css('button'));
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+    //    On peut tester les Inputs en définissant simplement des valeurs sur les propriétés d'entrée d'un composant.
+    it('should Button send be disabled if input enabled has false', () => {
+        component.enabled = false;
+        fixture.detectChanges();
+        expect(submitEl.nativeElement.disabled).toBeTruthy();
+    });
+    // On peut tester les Outputs en souscrivant à un observable EventEmitters et en stockant les valeurs émises sur des variables locales.
+    it('should output return object userModel.. click on login ', () => {
+
+        let userModel: UserModel;
+        usernameEl.nativeElement.value = 'iraouf.soft@gmail.com';
+        passwordEl.nativeElement.value = 'P@ss?@';
+
+        component.loggedIn.subscribe((val) => userModel = val);
+        submitEl.triggerEventHandler('click', null);
+
+        expect(userModel.username).toBe('iraouf.soft@gmail.com');
+        expect(userModel.password).toBe('P@ss?@');
+    });
+});
+
--- src/app/9-testing/testing.module.ts
+++ src/app/9-testing/testing.module.ts
@@ -11,12 +11,13 @@ import {WithMocksServiceComponent} from './2-testing-with-mocks-spies/with-mocks
 import {WithSpiesComponent} from './2-testing-with-mocks-spies/with-spies/with-spies.component';
 import {TestBedComponent} from './3-test-bed/test-bed.component';
 import {TestbedAsyncComponent} from './4-testbed-async/testbed-async.component';
+import { ComponentComponent } from './5-component/component.component';
 
 
 @NgModule({
     imports: [CommonModule, HttpClientModule, FormsModule, ReactiveFormsModule, TestingRoutingModule],
     declarations: [DashboardComponent, CurrencyToKMBPipe, WithRealServiceComponent, WithMocksServiceComponent,
-        WithSpiesComponent, TestBedComponent, TestbedAsyncComponent],
+        WithSpiesComponent, TestBedComponent, TestbedAsyncComponent, ComponentComponent],
     providers: [AppStateService]
 })
 
